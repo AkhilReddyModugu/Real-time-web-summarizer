5eb3e0a73dd570d98247c33cecbd75d12d3da349
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2025-01-01 16:09:29 +0530

    Minor changes in server logic

--- server/server.py
+++ server/server.py
@@ -15,7 +15,6 @@ from utils.web_search import (
     clean_text_corpus,
     llm_summarize,
     save_links_to_file,
-    fetch_image_url
 )
 
 load_dotenv()
@@ -58,53 +57,40 @@ async def read_root():
 async def summarize(search_request: SearchRequest):
     search_string = search_request.query
     length = search_request.length
-    llm = "Gemini" 
     
     print(f"Searching for: {search_string}")
+    
     try:
         start = time.time()
         search_result = await search_query(search_string, length, API_KEY, SEARCH_ENGINE_ID)
+    
         links = extract_links(search_result)
+        
         save_links_to_file(links)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=f"Error during search or link extraction: {str(e)}")
     
     print(f"Found {len(links)} links")
 
-    # Set the failure threshold
-    failure_threshold = 3
-    failure_count = 0
-    results = []
-
-    with ThreadPoolExecutor() as executor:
-        future_to_link = {executor.submit(fetch_data, link): link for link in links}
-        for future in as_completed(future_to_link):
-            link = future_to_link[future]
-            try:
-                result = future.result()
-                results.append(result)
-            except Exception as e:
-                print(f"Fetching failed for {link}")
-                # print(f"Fetching failed for {link}: {e}")
-                failure_count += 1
-                # Check if failure threshold is exceeded
-                if failure_count >= failure_threshold:
-                    print("Failure threshold exceeded. Terminating process.")
-                    raise HTTPException(status_code=500, detail="Failed to fetch sufficient data from the internet. Please try again later or try with a more specific query.")
+    # Fetch data from all links concurrently using asyncio.gather
+    tasks = [fetch_and_extract_paragraphs(link) for link in links]
+    try:
+        results = await asyncio.gather(*tasks)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during data fetching: {str(e)}")
 
     text_corpus = "\n".join(filter(None, results))
     cleaned_text = clean_text_corpus(text_corpus)
-    print("Time for scraping: ", time.time()-start)
+    
+    print("Time for scraping: ", time.time() - start)
 
     summary = llm_summarize(text=cleaned_text, search_query=search_string, length=length)
 
     with open("summary.txt", 'w') as f:
         f.write(summary)
 
-    image_url = await fetch_image_url(search_string, API_KEY, SEARCH_ENGINE_ID)
-    print(image_url)
-    
-    return {"summary": summary, "image_url": image_url}
+    return {"summary": summary}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
